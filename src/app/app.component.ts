fe06afad22573c6d25400d0fed875a00fa66f72b
Srinath <srinath.sathianarayanan@iff.com>
2018-08-07 18:51:57 +0530

    accordion style updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularWaitBarrier } from '../../node_modules/blocking-proxy/built/lib/angular_wait_barrier';
 
 @Component({
   selector: 'app-root',
@@ -7,39 +8,147 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  // chartParams = {
+  //   'colors': ['#2dadde', '#3565c8', '#0c2b75', '#ddd'],
+  //   'data': [
+  //     {
+  //       'benchmark': {
+  //         'name': 'hello benchmark',
+  //         'values': [4.75, 2.25, .75, 1.25]
+  //       }
+  //     },
+  //     {
+  //       'groupName': 'Group 1',
+  //       'evaluations': [
+  //         {
+  //           'name': '7803 - SB16 Sprayed Sample Nozzle',
+  //           'values': [5.85, 1, 1.15, 2],
+  //         }, {
+  //           'name': '7801 - SB16 Fresh Scent',
+  //           'values': [5.35, 2.65, .5, 1.5],
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       'groupName': 'Group 2',
+  //       'evaluations': [
+  //         {
+  //           'name': '7802 - SB16 Sprayed Sample Nozzle',
+  //           'values': [4.55, 3.45, 1, 1],
+  //         }, {
+  //           'name': '7800 - SB16 Fresh Scent',
+  //           'values': [3.75, 3.75, 1.5, 1],
+  //         }
+  //       ]
+  //     }]
+  // };
+
   chartParams = {
-    'colors': ['#2dadde', '#3565c8', '#0c2b75'],
+    'colors': ['#faa838'],
     'data': [
       {
         'benchmark': {
           'name': 'hello benchmark',
-          'values': [8.5, 1.5, 2, 3],
-          'total': 15
+          'values': [4.75]
         }
       },
       {
         'groupName': 'Group 1',
-        'answers': [
+        'evaluations': [
           {
             'name': '7803 - SB16 Sprayed Sample Nozzle',
-            'values': [5.8, 2.5, 1.0],
+            'values': [3.85],
+            'evaluators': [{
+              'name': 'Colin Ochel',
+              'response': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, optio.',
+              'values': [2.85]
+            }, {
+              'name': 'Marco van Wagenberg',
+              'response': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad corrupti quia, quae cupiditate',
+              'values': [3.45]
+            }, {
+              'name': 'Colin Ochel',
+              'response': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, optio.',
+              'values': [2.55]
+            }, {
+              'name': 'Marco van Wagenberg',
+              'response': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad corrupti quia, quae cupiditate',
+              'values': [4.15]
+            }]
           }, {
             'name': '7801 - SB16 Fresh Scent',
-            'values': [5.75, 1.54, 2.5],
+            'values': [3.35],
+            'evaluators': [{
+              'name': 'Colin Ochel',
+              'response': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, optio.',
+              'values': [2.85]
+            }, {
+              'name': 'Marco van Wagenberg',
+              'response': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad corrupti quia, quae cupiditate',
+              'values': [3.45]
+            }, {
+              'name': 'Colin Ochel',
+              'response': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, optio.',
+              'values': [2.55]
+            }, {
+              'name': 'Marco van Wagenberg',
+              'response': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad corrupti quia, quae cupiditate',
+              'values': [4.15]
+            }]
           }
         ]
       },
       {
         'groupName': 'Group 2',
-        'answers': [
+        'evaluations': [
           {
-            'name': '7802 - SB16 Sprayed Sample Nozzle',
-            'values': [5.8, 4.0, 2.1],
+            'name': '7802 - Lush scent',
+            'values': [4.55],
+            'evaluators': [{
+              'name': 'Colin Ochel',
+              'response': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, optio.',
+              'values': [2.85]
+            }, {
+              'name': 'Marco van Wagenberg',
+              'response': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad corrupti quia, quae cupiditate',
+              'values': [3.45]
+            }, {
+              'name': 'Colin Ochel',
+              'response': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, optio.',
+              'values': [2.55]
+            }, {
+              'name': 'Marco van Wagenberg',
+              'response': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad corrupti quia, quae cupiditate',
+              'values': [4.15]
+            }]
           }, {
-            'name': '7800 - SB16 Fresh Scent',
-            'values': [5.25, 3.5, 0.75],
+            'name': '7800 - Not sprayed',
+            'values': [3.75],
+            'evaluators': [{
+              'name': 'Colin Ochel',
+              'response': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, optio.',
+              'values': [2.85]
+            }, {
+              'name': 'Marco van Wagenberg',
+              'response': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad corrupti quia, quae cupiditate',
+              'values': [3.45]
+            }, {
+              'name': 'Colin Ochel',
+              'response': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, optio.',
+              'values': [2.55]
+            }, {
+              'name': 'Marco van Wagenberg',
+              'response': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad corrupti quia, quae cupiditate',
+              'values': [4.15]
+            }]
           }
         ]
       }]
   };
+
+  benchmarkTotal = this.chartParams.data[0].benchmark.values.reduce((a, b) => a + b, 0);
+
+  toggleEvaluations(element) {
+    element.currentTarget.nextElementSibling.classList.add('active');
+  }
 }
